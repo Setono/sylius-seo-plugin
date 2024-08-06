e664b8ca59bb45d18f185778d23a86ab0820d922
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-06 10:36:31 +0200

    Add todo and more auto configuration

--- src/DependencyInjection/SetonoSyliusSEOExtension.php
+++ src/DependencyInjection/SetonoSyliusSEOExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Setono\SyliusSEOPlugin\DependencyInjection;
 
+use Setono\SyliusSEOPlugin\DataMapper\OnlineStore\OnlineStoreDataMapperInterface;
 use Setono\SyliusSEOPlugin\DataMapper\Product\ProductDataMapperInterface;
 use Setono\SyliusSEOPlugin\DataMapper\ProductGroup\ProductGroupDataMapperInterface;
+use Setono\SyliusSEOPlugin\DataMapper\Website\WebsiteDataMapperInterface;
 use Setono\SyliusSEOPlugin\UrlGenerator\ProductVariantUrlGeneratorInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -34,6 +36,11 @@ final class SetonoSyliusSEOExtension extends Extension implements PrependExtensi
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $container
+            ->registerForAutoconfiguration(OnlineStoreDataMapperInterface::class)
+            ->addTag('setono_sylius_seo.online_store_data_mapper')
+        ;
+
+        $container
             ->registerForAutoconfiguration(ProductDataMapperInterface::class)
             ->addTag('setono_sylius_seo.product_data_mapper')
         ;
@@ -41,6 +48,11 @@ final class SetonoSyliusSEOExtension extends Extension implements PrependExtensi
         $container
             ->registerForAutoconfiguration(ProductGroupDataMapperInterface::class)
             ->addTag('setono_sylius_seo.product_group_data_mapper')
+        ;
+
+        $container
+            ->registerForAutoconfiguration(WebsiteDataMapperInterface::class)
+            ->addTag('setono_sylius_seo.website_data_mapper')
         ;
 
         $container->setAlias(ProductVariantUrlGeneratorInterface::class, $config['product_variant_url_generator']);
@@ -106,6 +118,7 @@ final class SetonoSyliusSEOExtension extends Extension implements PrependExtensi
             return;
         }
 
+        // todo this will give an error if the search_url_template isn't set
         $loader->load('services/structured_data/website.xml');
 
         if (isset($config['search_url_template'])) {
