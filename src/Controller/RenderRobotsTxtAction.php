ec4a71d1a1430d9a3874575416efe7a7d261af9a
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-20 10:20:52 +0200

    Try to fix Psalm error

--- src/Controller/RenderRobotsTxtAction.php
+++ src/Controller/RenderRobotsTxtAction.php
@@ -7,6 +7,7 @@ namespace Setono\SyliusSEOPlugin\Controller;
 use Setono\SyliusSEOPlugin\Model\ChannelInterface;
 use Setono\SyliusSEOPlugin\Renderer\RobotsTxtRendererInterface;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Model\ChannelInterface as BaseChannelInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Webmozart\Assert\Assert;
@@ -22,6 +23,7 @@ final class RenderRobotsTxtAction
 
     public function __invoke(): Response
     {
+        /** @var ChannelInterface|BaseChannelInterface $channel */
         $channel = $this->channelContext->getChannel();
         Assert::isInstanceOf($channel, ChannelInterface::class);
 
