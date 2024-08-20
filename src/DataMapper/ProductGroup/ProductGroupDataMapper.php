7ffd2e37831af13115cf29237987a22494307491
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-20 09:56:48 +0200

    Add sanitizeString function instead

--- src/DataMapper/ProductGroup/ProductGroupDataMapper.php
+++ src/DataMapper/ProductGroup/ProductGroupDataMapper.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Setono\SyliusSEOPlugin\DataMapper\ProductGroup;
 
+use function Setono\SyliusSEOPlugin\sanitizeString;
 use Setono\SyliusSEOPlugin\StructuredData\Thing\Product\ProductGroup;
 use Sylius\Component\Core\Model\ProductInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use function Symfony\Component\String\u;
 
 final class ProductGroupDataMapper implements ProductGroupDataMapperInterface
 {
@@ -17,12 +17,10 @@ final class ProductGroupDataMapper implements ProductGroupDataMapperInterface
 
     public function map(ProductInterface $product, ProductGroup $productGroup): void
     {
-        $description = $product->getDescription();
-
         $productGroup->name = $product->getName();
         // truncates at 5,000 characters because of this rather random suggestion:
         // https://support.google.com/webmasters/thread/195641191/invalid-string-length-in-field-description?hl=en
-        $productGroup->description = null === $description ? null : u(strip_tags($description))->trim()->truncate(5000)->toString();
+        $productGroup->description = sanitizeString($product->getDescription(), true, 5000);
         $productGroup->productGroupID = $product->getCode();
 
         $productGroup->url = $this->urlGenerator->generate(
--- src/Resources/functions.php
+++ src/Resources/functions.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Setono\SyliusSEOPlugin;
 
+use function Symfony\Component\String\u;
+
 /** @psalm-suppress UndefinedClass,MixedArgument */
 if (!\function_exists(formatAmount::class)) {
     function formatAmount(?int $amount): float
@@ -13,5 +15,26 @@ if (!\function_exists(formatAmount::class)) {
         }
 
         return round($amount / 100, 2);
+    }
+}
+
+/** @psalm-suppress UndefinedClass,MixedArgument */
+if (!\function_exists(sanitizeString::class)) {
+    function sanitizeString(?string $string, bool $stripTags = true, int $maxLength = null): ?string
+    {
+        if (null === $string) {
+            return null;
+        }
+
+        if ($stripTags) {
+            $string = strip_tags($string);
+        }
+
+        $s = u($string)->trim();
+        if (null !== $maxLength) {
+            $s = $s->truncate($maxLength);
+        }
+
+        return $s->toString();
     }
 }
