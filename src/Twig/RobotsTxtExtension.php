e3e45501db1862e7f8115a11680b9be2c1b4a728
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-24 14:42:37 +0100

    Enhance robots.txt file handling with readability check
    Added an `is_readable` check to ensure the `robots.txt` file can be accessed safely. This improves robustness by preventing potential errors when the file exists but is not readable.

--- src/Twig/RobotsTxtExtension.php
+++ src/Twig/RobotsTxtExtension.php
@@ -28,8 +28,10 @@ final class RobotsTxtExtension extends AbstractExtension
      */
     public function existingRobotsTxt(): ?string
     {
-        if (file_exists($this->publicDirectory . '/robots.txt')) {
-            return file_get_contents($this->publicDirectory . '/robots.txt');
+        $filename = $this->publicDirectory . '/robots.txt';
+
+        if (file_exists($filename) && is_readable($filename)) {
+            return file_get_contents($filename);
         }
 
         return null;
