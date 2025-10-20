f1873f1279c92237e66d71efa622daa011f4c729
Joachim Løvgaard <joachim@loevgaard.dk>
2025-10-20 08:43:41 +0200

    Cap the length of name and also set the description

--- src/DataMapper/Product/ProductDataMapper.php
+++ src/DataMapper/Product/ProductDataMapper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\SyliusSEOPlugin\DataMapper\Product;
 
+use function Setono\SyliusSEOPlugin\sanitizeString;
 use Spatie\SchemaOrg\Product;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 
@@ -12,7 +13,8 @@ final class ProductDataMapper implements ProductDataMapperInterface
     public function map(ProductVariantInterface $productVariant, Product $product): void
     {
         $product
-            ->name(trim(strip_tags((string) ($productVariant->getName() ?? $productVariant->getProduct()?->getName()))))
+            ->name((string) sanitizeString($productVariant->getName() ?? $productVariant->getProduct()?->getName(), true, 70))
+            ->description((string) sanitizeString($productVariant->getProduct()?->getDescription(), true, 5000))
             ->sku((string) $productVariant->getCode())
         ;
     }
