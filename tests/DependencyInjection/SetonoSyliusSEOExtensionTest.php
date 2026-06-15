954a86eb9e3302772c4f608a0d7eef73a37a9bb1
Joachim Løvgaard <joachim@loevgaard.dk>
2026-06-15 11:57:44 +0200

    Fix static analysis on Symfony 7.4 lowest deps and cover the twig hooks prepend
    - Ignore missingType.generics in Configuration (TreeBuilder/ArrayNodeDefinition are generic in Symfony >=7.1 but not 6.4)
    - Add a test asserting the sylius_twig_hooks prepend so the prepend() method is covered

--- tests/DependencyInjection/SetonoSyliusSEOExtensionTest.php
+++ tests/DependencyInjection/SetonoSyliusSEOExtensionTest.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Setono\SyliusSEOPlugin\Tests\DependencyInjection;
 
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use PHPUnit\Framework\Attributes\Test;
 use Setono\SyliusSEOPlugin\DependencyInjection\SetonoSyliusSEOExtension;
 use Setono\SyliusSEOPlugin\UrlGenerator\ProductVariantUrlGenerator;
 use Setono\SyliusSEOPlugin\UrlGenerator\ProductVariantUrlGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * See examples of tests and configuration options here: https://github.com/SymfonyTest/SymfonyDependencyInjectionTest
@@ -21,11 +23,30 @@ final class SetonoSyliusSEOExtensionTest extends AbstractExtensionTestCase
         ];
     }
 
-    #[\PHPUnit\Framework\Attributes\Test]
+    #[Test]
     public function after_loading_the_correct_parameter_has_been_set(): void
     {
         $this->load();
 
         $this->assertContainerBuilderHasAlias(ProductVariantUrlGeneratorInterface::class, ProductVariantUrlGenerator::class);
+    }
+
+    #[Test]
+    public function it_prepends_twig_hooks_for_the_seo_output(): void
+    {
+        $container = new ContainerBuilder();
+
+        (new SetonoSyliusSEOExtension())->prepend($container);
+
+        $config = $container->getExtensionConfig('sylius_twig_hooks');
+
+        self::assertCount(1, $config);
+
+        /** @var array{ hooks: array<string, mixed> } $hooksConfig */
+        $hooksConfig = $config[0];
+
+        self::assertArrayHasKey('sylius_shop.base#metatags', $hooksConfig['hooks']);
+        self::assertArrayHasKey('sylius_admin.channel.create.content.form.sections', $hooksConfig['hooks']);
+        self::assertArrayHasKey('sylius_admin.channel.update.content.form.sections', $hooksConfig['hooks']);
     }
 }
